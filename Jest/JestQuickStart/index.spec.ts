dd2367fd1e1b08883ad74068fb5cf966dedf53aa
PhilipSung <spinnavor@naver.com>
2024-11-28 01:30:20 +0900

    feature: add jest directory

---
+++ Jest/JestQuickStart/index.spec.ts
@@ -0,0 +1,7 @@
+import { addFunction } from "./index";
+
+describe("feature::news", () => {
+  it("JestQuickStart", () => {
+    expect(addFunction(1, 2)).toBe(3);
+  });
+});
---
+++ Jest/JestQuickStart/index.ts
@@ -0,0 +1,3 @@
+export const addFunction = (a: number, b: number) => {
+  return a + b;
+};
